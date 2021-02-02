7c840017efecce01374eb4d22b0af1b12627d6e4
a-powers <awpowers@gmail.com>
2021-02-02 16:30:02 +0000

    update

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from art import logo
 
 print(logo)
 
-name = input("What is your name?\n")
-bid = int(input("What is your bid?\n$"))
-
 bid_listing = {}
-bid_listing[name] = bid
+
+bidding_finished = False
+
+while not bidding_finished:
+    name = input("What is your name?\n")
+    bid = int(input("What is your bid?\n$"))
+    bid_listing[name] = bid
 
 more_bids = input("Are there more bidders?\nPress 'y' or 'n'")
 
-bid_final = False
-while not all_bidding():
-    if more_bids == 'y':
-        
+
