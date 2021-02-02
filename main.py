7dfa4373ce01ac5f4dcd71ea338bdb1bf170948d
a-powers <awpowers@gmail.com>
2021-02-02 22:53:07 +0000

    deletion

--- main.py
+++ main.py
@@ -1,17 +1,2 @@
 from replit import clear
 from art import logo
-
-print(logo)
-
-bid_listing = {}
-
-bidding_finished = False
-
-while not bidding_finished:
-    name = input("What is your name?\n")
-    bid = int(input("What is your bid?\n$"))
-    bid_listing[name] = bid
-
-more_bids = input("Are there more bidders?\nPress 'y' or 'n'")
-
-
